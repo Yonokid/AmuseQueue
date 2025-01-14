d3d0eaf9fb82140088ef6149030b59f93a1cd1ff
Yonokid <37304577+Yonokid@users.noreply.github.com>
2025-01-13 22:06:25 -0500

    Update game_queue.py

--- game_queue.py
+++ game_queue.py
@@ -3,6 +3,6 @@ from app.routes import init_routes
 
 app = create_app()
 socketio = get_socket()
-if __name__ == '__main__':
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+#if __name__ == '__main__':
+    #socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
 init_routes(app)
